3a7aba9a831ed1f968e87c16573ac76bcb685938
Clemich38 <clement.letreguilly@gmail.com>
2017-02-16 17:03:08 +0900

    remove unuseful "usings"

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,6 @@
 using System.Net.Http;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 
 namespace IPParser.Controllers
 {
